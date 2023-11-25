4a1498e9d8fca9026da9143eab21ede1fbff6791
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-11-25 10:17:43 -0300

    Added docs

--- smart-contract/contracts/GreatApe77Coin.sol
+++ smart-contract/contracts/GreatApe77Coin.sol
@@ -3,10 +3,34 @@ pragma solidity ^0.8.20;
 
 import {ERC20} from "./erc20/ERC20.sol";
 
-contract GreatApe77Coin is ERC20 {
+//  ______                                 __       ______                      ________ ________         ______            __
+// /      \                               |  \     /      \                    |        \        \       /      \          |  \
+//|  $$$$$$\  ______    ______    ______ _| $$_   |  $$$$$$\  ______    ______  \$$$$$$$$\$$$$$$$$      |  $$$$$$\ ______   \$$ _______
+//| $$ __\$$ /      \  /      \  |      \   $$ \  | $$__| $$ /      \  /      \    /  $$    /  $$       | $$   \$$/      \ |  \|       \
+//| $$|    \|  $$$$$$\|  $$$$$$\  \$$$$$$\$$$$$$  | $$    $$|  $$$$$$\|  $$$$$$\  /  $$    /  $$        | $$     |  $$$$$$\| $$| $$$$$$$\
+//| $$ \$$$$| $$   \$$| $$    $$ /      $$| $$ __ | $$$$$$$$| $$  | $$| $$    $$ /  $$    /  $$         | $$   __| $$  | $$| $$| $$  | $$
+//| $$__| $$| $$      | $$$$$$$$|  $$$$$$$| $$|  \| $$  | $$| $$__/ $$| $$$$$$$$/  $$    /  $$          | $$__/  \ $$__/ $$| $$| $$  | $$
+// \$$    $$| $$       \$$     \ \$$    $$ \$$  $$| $$  | $$| $$    $$ \$$     \  $$    |  $$            \$$    $$\$$    $$| $$| $$  | $$
+//  \$$$$$$  \$$        \$$$$$$$  \$$$$$$$  \$$$$  \$$   \$$| $$$$$$$   \$$$$$$$\$$      \$$              \$$$$$$  \$$$$$$  \$$ \$$   \$$
+//                                                          | $$
+//                                                          | $$
+//                                                           \$$
 
+/**
+ * @title GreatApe77 Coin
+ * @author Mateus Navarro
+ * @notice Implementacao do token GreatApe77 Coin
+ */
+contract GreatApe77Coin is ERC20 {
+    /**
+     * @notice Criador do contrato
+     */
     string public constant CREATOR = "Mateus Navarro";
 
+    /**
+     *
+     * @param initialSupplyReceiver Endereco que recebera a quantidade total de tokens
+     */
     constructor(
         address initialSupplyReceiver
     )
