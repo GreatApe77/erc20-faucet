e99c8102184cb2afe48fc9aa4c506b086782aac7
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-11-26 17:30:18 -0300

    adiciona testes

--- smart-contract/test/FaucetBank.test.ts
+++ smart-contract/test/FaucetBank.test.ts
@@ -29,5 +29,48 @@ describe("FaucetBank", () => {
         const currentERC20Address = await faucetBank.greatApe77Coin()
         expect(currentERC20Address).to.be.equal(newDummyERC20Address);
     })
-  
+    it("should check initial claim amount 7 * 10 **18 (7 ether)", async () => {
+        const { faucetBank, accounts,erc20 } = await loadFixture(deployFaucetBankFixture);
+        const claimAmount = await faucetBank.claimAmount()
+        expect(claimAmount).to.be.equal(ethers.parseEther("7"));
+    })
+    it("should check initial claim amount 2 minutes", async () => {
+        const { faucetBank, accounts,erc20 } = await loadFixture(deployFaucetBankFixture);
+        const claimInterval = await faucetBank.claimInterval()
+        const TWO_MINUTES = 120
+        expect(claimInterval).to.be.equal(TWO_MINUTES);
+    })
+    it("should set claim amount", async () => {
+        const { faucetBank, accounts,erc20 } = await loadFixture(deployFaucetBankFixture);
+        const claimAmount = ethers.parseEther("77")
+        await faucetBank.setClaimAmount(claimAmount)
+        const currentClaimAmount = await faucetBank.claimAmount()
+        expect(currentClaimAmount).to.be.equal(claimAmount);
+    })
+    it("should not set claim amount (not owner)", async () => {
+        const { faucetBank, accounts,erc20 } = await loadFixture(deployFaucetBankFixture);
+        const claimAmount = ethers.parseEther("77")
+        await expect(faucetBank.connect(accounts[1]).setClaimAmount(claimAmount)).to.be.revertedWithCustomError(faucetBank,"Ownable__notOwner")
+    })
+    it("should set claim interval", async () => {
+        const { faucetBank, accounts,erc20 } = await loadFixture(deployFaucetBankFixture);
+        const THREE_MINUTES = 180
+        const claimInterval = THREE_MINUTES
+        await faucetBank.setClaimIntervalInSeconds(claimInterval)
+        const currentClaimInterval = await faucetBank.claimInterval()
+        expect(currentClaimInterval).to.be.equal(claimInterval);
+    })
+    it("should not set claim interval (not owner)", async () => {
+        const { faucetBank, accounts,erc20 } = await loadFixture(deployFaucetBankFixture);
+        const THREE_MINUTES = 180
+        const claimInterval = THREE_MINUTES
+        await expect(faucetBank.connect(accounts[1]).setClaimIntervalInSeconds(claimInterval)).to.be.revertedWithCustomError(faucetBank,"Ownable__notOwner")
+    })
+    it("should not set token address (not owner)", async () => {
+        const { faucetBank, accounts,erc20 } = await loadFixture(deployFaucetBankFixture);
+        const DummyERC20Factory = await ethers.getContractFactory("DummyERC20")
+        const newDummyERC20 = await DummyERC20Factory.deploy("Dummy ERC20", "DERC20",ethers.parseEther("777"),accounts[0].address);
+        const newDummyERC20Address = await newDummyERC20.getAddress()
+        await expect(faucetBank.connect(accounts[1]).setTokenAddress(newDummyERC20Address)).to.be.revertedWithCustomError(faucetBank,"Ownable__notOwner")
+    })
 })
