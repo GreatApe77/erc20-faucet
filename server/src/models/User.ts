758887fcfe8e214ca86a987efd02ffd69bcc1335
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-11-29 18:30:32 -0300

    adiciona senha ao Usuario

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose"
 
 const userSchema = new mongoose.Schema({
     nickname: {type: String, required: true},
+    password: {type: String, required: true},
     custodyAccountPublicKey: {type: String, required: true},
     custodyAccountPrivateKey: {type: String, required: true},
     lastClaimed: {type: Date, required: true},
