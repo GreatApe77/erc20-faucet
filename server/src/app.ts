95868f5d01024203a10280b1dbaf3c1463a6f6ec
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-11-29 18:11:03 -0300

    rodando o servidor

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,11 @@
-console.log("Hello App")
+import express from "express";
+import morgan from "morgan";
+import cors from "cors"
+const app = express();
+
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(cors())
+
+
+export default app;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello index")
+import app from "./app";
+import dotenv from "dotenv"
+dotenv.config()
+
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Listening: ${PORT}`);
+});
+
