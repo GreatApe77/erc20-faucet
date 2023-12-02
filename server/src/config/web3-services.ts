c0bc5a32c3b074932460df9eacef00c8744712b5
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-12-02 11:17:33 -0300

    typed contracts

--- server/src/config/web3-services.ts
+++ server/src/config/web3-services.ts
@@ -1,8 +1,29 @@
-import {ethers} from "ethers"
-import dotenv from "dotenv"
-import { FAUCET_BANK_ABI,FAUCET_BANK_CONTRACT_ADDRESS,ERC20_TOKEN_ABI,ERC20_TOKEN_CONTRACT_ADDRESS } from "./contract-connection"
-dotenv.config()
-const provider = new ethers.JsonRpcProvider(`${process.env.RPC_URL}`)
+import { ethers } from "ethers";
+import dotenv from "dotenv";
+import {
+	ERC20_TOKEN_CONTRACT_ADDRESS,
+	FAUCET_BANK_CONTRACT_ADDRESS,
+} from "./contract-connection";
+import { FaucetBank, GreatApe77Coin } from "../contract-types";
 
-const wallet = new ethers.Wallet(`${process.env.PRIVATE_KEY}`, provider)
+dotenv.config();
+const provider = new ethers.JsonRpcProvider(`${process.env.RPC_URL}`);
 
+const wallet = new ethers.Wallet(`${process.env.PRIVATE_KEY}`, provider);
+
+const faucetBank = new ethers.Contract(
+	FAUCET_BANK_CONTRACT_ADDRESS,
+	require("../abis/FaucetBank.json"),
+	wallet
+) as unknown as FaucetBank;
+const greatApe77Coin = new ethers.Contract(
+	ERC20_TOKEN_CONTRACT_ADDRESS,
+	require("../abis/GreatApe77Coin.json"),
+	wallet
+) as unknown as GreatApe77Coin;
+
+
+faucetBank.owner().then((owner) => {
+    console.log("Faucet Bank owner:", owner);
+}
+);
