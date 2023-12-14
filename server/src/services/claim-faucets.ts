ba9770d5eeb499682b6f4f8059a133d0a3f68464
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-12-14 18:06:06 -0300

    documenta um service

--- server/src/services/claim-faucets.ts
+++ server/src/services/claim-faucets.ts
@@ -1,5 +1,12 @@
 import { faucetBankInstance } from "../config/web3-services";
 
+/**
+ * Resgata os faucets para um endereço.
+ * 
+ * @param address - Endereço para o qual os faucets serão resgatados.
+ * @returns uma Promise de um objeto com a hash da transação.
+ * @throws um erro caso ocorra algum problema na transação.
+ */
 export async function claimFaucets(address:string){
     try {
         const response = await faucetBankInstance.claimFaucets(address)
