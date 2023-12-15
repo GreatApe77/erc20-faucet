f6a6211bd580a962e78d39fee75daae9537ffb00
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-12-14 21:22:47 -0300

    finaliza o beck por enquanto

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,9 +4,16 @@ import cors from "cors"
 import router from "./router";
 const app = express();
 
+
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cors())
+
+app.get("/health",(req,res)=>{
+    return res.status(200).json({
+        message: "Health is OK!"
+    })
+})
 app.use(router)
 
 export default app;
