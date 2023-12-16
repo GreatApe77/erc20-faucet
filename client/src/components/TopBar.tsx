d1c5e101fd36fb1446a2c8c723aa8f9c97265bbd
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-12-16 16:37:14 -0300

    Define comportamento responsivo do Header

--- client/src/components/TopBar.tsx
+++ client/src/components/TopBar.tsx
@@ -1,34 +1,81 @@
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import { Avatar } from "@mui/material";
-export default function TopBar() {
-	return (
-		<AppBar color="transparent" position="static">
-			<Toolbar>
-				{/* Left side - Logo and Name */}
-				<IconButton edge="start" color="inherit" aria-label="menu">
-					<Avatar src="/great-ape-psn.png" />
-				</IconButton>
-				<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-					GreatApe77 Faucets
-				</Typography>
+import React, { useState } from 'react';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+  useMediaQuery,
+  Avatar,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  Box,
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
-				{/* Right side - Buttons */}
-				<Button color="primary" variant="contained">
-					Login
-				</Button>
-				<Button color="primary" variant="text">
-					Register
-				</Button>
-				<Button color="info" variant="outlined">
-					Connect Wallet
-				</Button>
-			</Toolbar>
-		</AppBar>
-	);
+export default function TopBar() {
+  const isMobile = useMediaQuery('(max-width:600px)');
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setDrawerOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setDrawerOpen(false);
+  };
+
+  return (
+    <AppBar color="transparent" position="static">
+      <Toolbar sx={{ justifyContent: 'space-between' }}>
+        {/* Left side - Logo and Name */}
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <Avatar src="/great-ape-psn.png" />
+          </IconButton>
+          <Typography variant="h6" component="div">
+            GreatApe77 Faucets
+          </Typography>
+        </Box>
+
+        {isMobile ? (
+          // If mobile, display hamburger menu on the right
+          <IconButton edge="end" color="inherit" aria-label="menu" onClick={handleDrawerOpen}>
+            <MenuIcon />
+          </IconButton>
+        ) : (
+          // If not mobile, display buttons on the right
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Button color="primary" variant="contained">
+              Login
+            </Button>
+            <Button color="primary" variant="text">
+              Register
+            </Button>
+            <Button color="info" variant="outlined">
+              <Avatar src="/MetaMask_Fox.svg" />
+              Connect Wallet
+            </Button>
+          </Box>
+        )}
+
+        {/* Drawer for mobile */}
+        <Drawer anchor="right" open={drawerOpen} onClose={handleDrawerClose}>
+          <List>
+            <ListItem button onClick={handleDrawerClose}>
+              <ListItemText primary="Login" />
+            </ListItem>
+            <ListItem button onClick={handleDrawerClose}>
+              <ListItemText primary="Register" />
+            </ListItem>
+            <ListItem button onClick={handleDrawerClose}>
+              <ListItemText primary="Connect Wallet" />
+            </ListItem>
+          </List>
+        </Drawer>
+      </Toolbar>
+    </AppBar>
+  );
 }
