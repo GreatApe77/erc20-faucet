32cb4dc9f23b367a922324398e5da82328256afe
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-12-23 12:58:11 -0300

    cria o me service

---
+++ client/src/services/me.ts
@@ -0,0 +1,22 @@
+import { SERVER_URL } from "../constants/server-url"
+import { ApiResponse } from "../types/ApiResponse"
+
+
+export async function meService(token: string) {
+   try {
+        const response = await fetch(`${SERVER_URL}/users/me`,{
+            headers:{
+                authorization: `Bearer ${token}`
+            }
+        })
+        const data = await response.json()
+        return {
+            status: response.status,
+            data
+        } as ApiResponse
+   } catch (error:any) {
+        console.error(error)
+        throw new Error("Error cwhile trying to get user")
+    }
+    
+}
