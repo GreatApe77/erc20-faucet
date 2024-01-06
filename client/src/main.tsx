53c2acbb9032316450ebb73696c9972ef1c87cd7
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-01-06 19:15:08 -0300

    changed theme

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,21 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { CssBaseline } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+
+import { ThemeProvider } from "@mui/material";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { WalletProvider } from "./context/WalletContext.tsx";
 import { UserProvider } from "./context/UserContext.tsx";
+import theme from "./theme.tsx";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<CssBaseline>
-			<UserProvider>
-				<WalletProvider>
-					<App />
-				</WalletProvider>
-			</UserProvider>
+			<ThemeProvider theme={theme}>
+				<UserProvider>
+					<WalletProvider>
+						<App />
+					</WalletProvider>
+				</UserProvider>
+			</ThemeProvider>
 		</CssBaseline>
 	</React.StrictMode>
 );
