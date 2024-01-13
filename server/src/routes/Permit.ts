2e3ee311fbfb482cf9ae8ea0ed7ba96870ce188a
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-01-13 12:21:54 -0300

    pronto para testes manuais

--- server/src/routes/Permit.ts
+++ server/src/routes/Permit.ts
@@ -1,10 +1,11 @@
 import express from "express"
 
 import { authenticated } from "../middlewares/authenticated";
+import { permitController } from "../controllers/permit-controller";
 
 
 const router__Permit = express.Router();
 
-router__Permit.post("/")
+router__Permit.post("/",authenticated,permitController)
 
 export default router__Permit;
