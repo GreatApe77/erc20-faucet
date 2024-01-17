8b468efef511504ad47c0bc09230a8a2a94b5c98
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-01-17 16:09:16 -0300

    testar o router

--- server/src/router.ts
+++ server/src/router.ts
@@ -2,10 +2,12 @@ import express from "express"
 import router__User from "./routes/User";
 import router__Faucet from "./routes/Faucet";
 import router__Permit from "./routes/Permit";
+import router__Metamask from "./routes/metamask";
 const router = express.Router();
 
 
 router.use("/users",router__User)
 router.use("/faucets",router__Faucet)
 router.use("/permit",router__Permit)
+router.use("/metamask",router__Metamask)
 export default router;
